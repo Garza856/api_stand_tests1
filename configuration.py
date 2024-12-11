58eddb899cd0116ba1ba3f17d8171e6252653796
fabiola garza <fabiolagarza@MacBook-Air-de-fabiola.local>
2024-12-11 08:22:00 -0600

    Mi primer commit

---
+++ configuration.py
@@ -0,0 +1,10 @@
+URL_SERVICE = "https://cnt-6166cff3-8402-4cca-a703-dd036b0228ce.containerhub.tripleten-services.com"
+DOC_PATH = "/docs/"
+
+LOG_MAIN_PATH = "/api/logs/main/"
+USER_TABLE_PATH = "api/db/resource/user_model.csvs"
+
+CREATE_USER_PATH = "/api/v1/users/"
+
+PRODUCTS_KITS_PATH = "/api/v1/products/kits/"
+
---
+++ sender_stand_request.py
@@ -0,0 +1,41 @@
+import configuration
+import requests
+import data
+
+def post_new_user(body):
+    return requests.post(configuration.URL_SERVICE + configuration.CREATE_USER_PATH,  # inserta la dirección URL completa
+                         json=body,  # inserta el cuerpo de solicitud
+                         headers=data.headers)  # inserta los encabezados
+
+response_json= post_new_user(data.user_body).json()
+auth_token= response_json['authToken']
+print(auth_token)
+
+authorization = {
+    "Content-Type": "application/json",
+    "Authorization": f'Bearer {auth_token}'
+}
+
+
+def get_logs(count=20):
+   return requests.get(configuration.URL_SERVICE + configuration.LOG_MAIN_PATH,
+                        params={"count": count})
+
+
+
+
+def get_users_table():
+    return requests.get(configuration.URL_SERVICE + configuration.USER_TABLE_PATH)
+
+
+
+
+def post_products_kits(products_ids):
+    # Realiza una solicitud POST para buscar kits por productos.
+    return requests.post(configuration.URL_SERVICE + configuration.PRODUCTS_KITS_PATH,  # Concatenación de URL base y ruta.
+                         json=products_ids,  # Datos a enviar en la solicitud.
+                         headers=authorization)  # Encabezados de solicitud.
+
+
+
+
